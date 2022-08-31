4cbb035464ce283f224938a53deb387bf84e77f3
emanuel.sousa <emanuel.gabriel.sousa@hotmail.com>
2022-08-31 10:50:35 -0300

    Criando os atributos de CategoriaRequestDTO

--- src/main/java/br/com/emanuelgabriel/api/dtos/request/CategoriaRequestDTO.java
+++ src/main/java/br/com/emanuelgabriel/api/dtos/request/CategoriaRequestDTO.java
@@ -1,5 +1,15 @@
 package br.com.emanuelgabriel.api.dtos.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoriaRequestDTO {
+
+    private String nome;
+    private String descricao;
 
 }
