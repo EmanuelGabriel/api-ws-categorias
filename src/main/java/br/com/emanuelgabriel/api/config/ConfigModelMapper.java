2f6b6788146938e0b590b837134aeb2a18f1c52d
Emanuel Arcanjo Gabriel G. de Sousa <emanuel.sousa@tce.pi.gov.br>
2022-09-01 13:13:08 -0300

    Adicionando documentação com swagger da API de Categorias

--- src/main/java/br/com/emanuelgabriel/api/config/ConfigModelMapper.java
+++ src/main/java/br/com/emanuelgabriel/api/config/ConfigModelMapper.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-public class ModelMapperConfig {
+public class ConfigModelMapper {
 
 	@Bean
 	public ModelMapper modelMapper() {
---
+++ src/main/java/br/com/emanuelgabriel/api/config/ConfigSwagger.java
@@ -0,0 +1,16 @@
+package br.com.emanuelgabriel.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.webmvc.ui.SwaggerConfig;
+
+
+@OpenAPIDefinition(
+        info = @Info(description = "API externa de gerenciamento e controle de Categorias",
+                termsOfService = "Termos de serviço",
+                title = "Categorias - API",
+                version = "1.0.0",
+                contact = @Contact(name = "Emanuel Gabriel Sousa", email = "emanuel.gabriel.sousa@protonmail.com", url = "https://www.linkedin.com/in/emanuel-gabriel-sousa/")))
+public class ConfigSwagger extends SwaggerConfig {
+}
