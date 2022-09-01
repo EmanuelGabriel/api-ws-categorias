c495c51506217c26e87dca3c2e10494cbbf375ed
Emanuel Arcanjo Gabriel G. de Sousa <emanuel.sousa@tce.pi.gov.br>
2022-09-01 10:06:09 -0300

    Criando o atributo de dataAtualizacao de uma categoria

--- src/main/java/br/com/emanuelgabriel/api/domain/entity/Categoria.java
+++ src/main/java/br/com/emanuelgabriel/api/domain/entity/Categoria.java
@@ -1,35 +1,32 @@
 package br.com.emanuelgabriel.api.domain.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "categoria")
 public class Categoria implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long codigo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
 
-	@Column(name = "nome")
-	private String nome;
+    @Column(name = "nome")
+    private String nome;
 
-	@Column(name = "descricao")
-	private String descricao;
+    @Column(name = "descricao")
+    private String descricao;
 
-	@Column(name = "data_cadastro")
-	private LocalDateTime dataCadastro;
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
 
 }
